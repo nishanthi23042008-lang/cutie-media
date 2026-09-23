b4820fa4881bfe95915046502b5c952ff16bfe0a
Belle <330491187+nishanthi23042008-lang@users.noreply.github.com>
2026-09-23 20:52:09 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,12 @@
+// ==========================================
+// CUTIE MEDIA - SUPABASE CONNECTION
+// ==========================================
 
+const SUPABASE_URL = https:"//itoqtpdhpsvjxczxnvbz.supabase.co/rest/v1/";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_fqeE7-UBfgKmIkkx7_49Yw_TTFF2QYd";
+
+const cutieSupabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
